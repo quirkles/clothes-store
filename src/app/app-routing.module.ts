e11c74b5895a2ee6d0e4d0c779584fa57a02fa44
quirkles <al.quirk@gmail.com>
2019-11-12 19:59:11 -0500

    Bring in bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {StorefrontComponent} from './storefront/storefront.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: StorefrontComponent },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StorefrontComponent } from './storefront/storefront.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StorefrontComponent
   ],
   imports: [
     BrowserModule,
